12991c550ccd94f358e68aaf01d8b3305f88793b
Viktor <152012083+DimitrovViktor@users.noreply.github.com>
2025-09-01 22:19:00 +0100

    Update authentication route

--- cs-server-browser/src/App.tsx
+++ cs-server-browser/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import ServerListPage from './components/ServerListPage';
+import ServerListPage from './pages/ServerListPage';
 import AddServerPage from './pages/AddServerPage';
-import AuthPage from './pages/AuthPage';
+import Authentication from './components/Authentication';
 import { AuthProvider } from './context/AuthContext';
 import './style.css';
 
@@ -18,7 +18,7 @@ const App: React.FC = () => {
             <Route path="/" element={<ServerListPage />} />
             <Route path="/add-server" element={<AddServerPage />} />
             <Route path="/myservers" element={<AddServerPage />} />
-            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/auth" element={<Authentication />} />
           </Routes>
         </Router>
       </AuthProvider>
